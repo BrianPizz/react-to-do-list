f261436233e208076c2f4c5ce42e2c5a852fac69
BrianPizz <bpizzimentijr@outlook.com>
2023-10-26 19:43:17 -0700

    local storage

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,25 @@ import "./styles.css"
 import { TodoList } from "./TodoList"
 
 export default function App() {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem('ITEMS')
+    if (localValue == null) return []
 
-function addTodo(title) {
-  setTodos((currentTodos) => {
-    return [
-      ...currentTodos,
-      { id: crypto.randomUUID(), title, completed: false },
-    ]
+    return JSON.parse(localValue)
   })
-}
+
+  useEffect(() => {
+    localStorage.setItem('ITEM', JSON.stringify(todos))
+  }, [todos])
+
+  function addTodo(title) {
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title, completed: false },
+      ]
+    })
+  }
 
   function toggleTodo(id, completed) {
     setTodos(currentTodos => {
@@ -34,10 +43,10 @@ function addTodo(title) {
   }
 
   return (
-  <>
-    <NewTodoForm onSubmit={addTodo}/>
-    <h1 className="header">Todo List</h1>
-    <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
-  </>
+    <>
+      <NewTodoForm onSubmit={addTodo} />
+      <h1 className="header">Todo List</h1>
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+    </>
   )
 }
